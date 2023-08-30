56459c9067f2ea42a4c154b39ee792948101e425
Andre Setiawan A <anxdre@protonmail.com>
2023-08-30 11:06:13 +0700

    setup directory and add SplashScreen

---
+++ app/src/main/java/com/example/project1_160420110/view/splashscreen/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.example.project1_160420110.view.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.project1_160420110.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
