163a15ae15711af4205f0e26c78dc4dd5bf339fc
Andre Setiawan A <anxdre@protonmail.com>
2023-08-30 11:36:10 +0700

    add animation to splash screen

--- app/src/main/java/com/example/project1_160420110/view/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/example/project1_160420110/view/splashscreen/SplashScreen.kt
@@ -1,12 +1,31 @@
 package com.example.project1_160420110.view.splashscreen
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import com.example.project1_160420110.R
+import androidx.appcompat.app.AppCompatActivity
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
+import com.example.project1_160420110.databinding.ActivitySplashScreenBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        try {
+            YoYo.with(Techniques.BounceInUp)
+                .duration(1600)
+                .playOn(binding.tvTitle)
+
+            YoYo.with(Techniques.RollIn)
+                .duration(1000)
+                .playOn(binding.ivLogo)
+        }catch (e:Exception){
+            finish()
+        }
     }
 }
