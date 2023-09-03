ad6327fbfbe15bf08ac6b98fb3f80d39a91bc81e
Zufar Rachman <zufarafi02#gmail.com>
2023-09-03 20:55:35 +0700

    aman yee

--- app/src/main/java/com/example/project1_160420110/MainActivity.kt
+++ app/src/main/java/com/example/project1_160420110/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //160420110
+        //160420106
     }
 }
